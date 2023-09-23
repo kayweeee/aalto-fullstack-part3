c75c95ea94b1e8b3f0a0d7e93c7874c538233ddc
kayweeee <90853438+kayweeee@users.noreply.github.com>
2023-09-23 22:09:52 +0300

    completed: exercise 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+
+morgan.token("post-body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-body"
+  )
+);
 
 let persons = [
   {
